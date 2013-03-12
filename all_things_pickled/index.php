c319be319203c199c7a8f2a3e641d730e4b3043d
Alexander Schmitz <arschmitz@gmail.com>
2013-03-12 09:01:17 -0400

    Add individual pages

---
+++ all_things_pickled/index.php
@@ -0,0 +1,25 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/head.php"); ?>
+<div data-role="page">
+	<div data-role="header"><a href="#nav_panel" data-role="button" data-icon="bars">Menu</a>
+		<h2>Magic Dilly Beans</h2>
+	</div>
+	<div data-role="panel" id="nav_panel">
+		<ul data-role="listview">
+			<li data-theme="b"><a href="/"><img src="/img/home.png"><h1>Home</h1></a></li>
+			<li data-theme="d"><a href="/who_is_silly_dilly/"><img src="/img/silly_dilly.png"><h1>Who Is Silly Dilly?</h1></a></li>
+			<li data-theme="c"><a href="/magic_recipes/"><img src="/img/recipes.png"><h1>Magic Recipes</h1></a></li>
+            <li data-theme="a"><a href="/magical_contest/"><img src="/img/contest.png"><h1>Magical Contest</h1></a></li>
+            <li data-theme="b"><a href="/magic_dilly_beans_today/"><img src="/img/today.png"><h1>Magic Dilly Beans Today</h1></a></li>
+            <li data-theme="d"><a href="/product_partner_links/"><img src="/img/partners.png"><h1>Product Partner Links</h1></a></li>
+            <li data-theme="c"><a href="/buy_magic_dilly_beans/"><img src="/img/buy.png"><h1>Buy Magic Dilly Beans</h1></a></li>
+           <li data-theme="a"><a href="/contact_us/"><img src="/img/contact.png"><h1>Contact Us</h1></a></li>
+            <li data-theme="b"><a href="/distributor_login/"><img src="/img/block.png"><h1>Distributor Login</h1></a></li>
+            <li data-theme="d"><a href="/food_processing_links/"><img src="/img/block.png"><h1>Food Processing Links</h1></a></li>
+            <li data-theme="c"><a href="/the_truth_about_salt/"><img src="/img/block.png"><h1>The Truth About Salt</h1></a></li>
+            <li data-theme="a"><a href="/pouch_vs_jar/"><img src="/img/block.png"><h1>Pouch vs Jar</h1></a></li>
+		</ul>
+	</div>
+	<div data-role="content"></div>
+	<div data-role="footer"><h2>© All Rights Reserved Mad’s Magic Dilly Beans, LLC | 248 Northport Ave. | Belfast, Maine 04915 | (855) MY-DILLY (693-4559)</h2></div>
+</div>
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/bottom.php"); ?>
